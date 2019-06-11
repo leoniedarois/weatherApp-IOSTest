ae2bbedfda0dd855a7af36a287cc051832541ef3
Leo <leonie.grimoin@gmail.com>
2019-06-11 11:29:13 +0200

    Initial Commit

---
+++ weatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApplication
+//
+//  Created by Léo on 11/06/2019.
+//  Copyright © 2019 Léonie Grimoin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
