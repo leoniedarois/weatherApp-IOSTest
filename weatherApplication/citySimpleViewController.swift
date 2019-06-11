e55e36c453431cd01dccf1b28dbd87407be33c75
Leo <leonie.grimoin@gmail.com>
2019-06-11 12:16:34 +0200

    setting up the 2 views

---
+++ weatherApplication/citySimpleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  citySimpleViewViewController.swift
+//  weatherApplication
+//
+//  Created by Léo on 11/06/2019.
+//  Copyright © 2019 Léonie Grimoin. All rights reserved.
+//
+
+import UIKit
+
+class citySimpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
